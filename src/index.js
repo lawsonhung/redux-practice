a9123157a09eb403a76db6538f5b6fda6abb02d3
lawsonhung <lawsonhung@yahoo.com>
2020-05-24 18:57:24 -0400

    Finished Intro to Redux codealong Able to connect to store. Able to getState() and dispatch/update state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,131 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import logo from './logo.svg';
+import './App.css';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Install redux with:
+// $ npm i --save redux
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state={ count: 0 }, action) => {
+
+  console.log('Our reducers state: ', state);
+  console.log('Our reducers action: ', action);
+
+  switch (action.type) {
+    case 'UPDATE_COUNT':
+      return { count: action.count };
+
+    // case 'INCREMENT_COUNT':
+    //   return { count: state.count + 1 };
+
+    // // increment by 3
+    // case 'INCREMENT_BY_3':
+    //   return { count: state.count + 3 };
+
+    // // increment by 5
+    // case 'INCREMENT_BY_5':
+    //   return { count: state.count + 5 };
+
+    case 'INCREMENT_COUNT':
+      return { count: state.count + action.amount };
+
+    // case 'DECREMENT_COUNT':
+    //   return { count: state.count - 1 };
+
+    // // decrement by 3
+    // case 'DECREMENT_BY_3':
+    //   return { count: state.count - 3 };
+
+    // // decrement by 5
+    // case 'DECREMENT_BY_5':
+    //   return { count: state.count - 5 };
+
+    case 'DECREMENT_COUNT':
+      return { count: state.count - action.amount };
+
+    default:
+      return state;
+  };
+
+};
+
+const lawsonsStore = createStore(reducer);
+// debugger
+// Get state:
+// > lawsonsStore.getState()
+// Update state:
+// > lawsonsStore.dispatch({ type: 'INCREMENT_COUNT', amount: 3 })
+// > lawsonsStore.dispatch({ type: 'DECREMENT_COUNT', amount: 4 })
+// > lawsonsStore.getState()
+// -1
+
+// > lawsonsStore.dispatch({ type: 'UPDATE_COUNT', amount: 87 })
+// > lawsonsStore.getState()
+// 87
+
+class App extends Component {
+  // state = {
+  //   count: 0
+  // }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Counter />
+      </div>
+    )
+  }
+}
+
+class Header extends Component {
+  componentDidMount() {
+    lawsonsStore.subscribe(() => this.forceUpdate())
+  }
+
+  renderDescription = () => {
+    const remainder = lawsonsStore.getState().count % 5;
+    const upToNext = 5 - remainder;
+    return `The current count is less than ${lawsonsStore.getState().count + upToNext}`;
+  }
+
+  render() {
+    return (
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Welcome to React</h1>
+        <h3>{this.renderDescription() }</h3>
+      </header>
+    )
+  }
+}
+
+class Counter extends Component {
+  componentDidMount() {
+    lawsonsStore.subscribe(() => this.forceUpdate());
+  }
+
+  increment = () => {
+    // this.setState({ count: this.state.count + 1 });
+    lawsonsStore.dispatch({ type: 'INCREMENT_COUNT', amount: 1 });
+  }
+
+  decrement = () => {
+    // this.setState({ count: this.state.count - 1 });
+    lawsonsStore.dispatch({ type: 'DECREMENT_COUNT', amount: 1 });
+  }
+
+  render() {
+    return (
+      <div className='Counter'>
+        <h1>{lawsonsStore.getState().count}</h1>
+        <button onClick={this.decrement}> - </button>
+        <button onClick={this.increment}> + </button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
